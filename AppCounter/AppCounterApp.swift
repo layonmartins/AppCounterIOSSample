3ab0129732610b8638b555d23ecbe141e4afdf90
layonfonseca <layon.fonseca@eldorado.org.br>
2022-03-04 15:08:24 -0300

    Initial Commit

---
+++ AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by Layon Fonseca on 04/03/22.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppCounter
+//
+//  Created by Layon Fonseca on 04/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
