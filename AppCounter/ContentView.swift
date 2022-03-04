e7d7e74a9d79d330dddfc2c27fe139ef87151cee
layonfonseca <layon.fonseca@eldorado.org.br>
2022-03-04 17:42:17 -0300

    first commit

--- AppCounter/ContentView.swift
+++ AppCounter/ContentView.swift
@@ -7,10 +7,60 @@
 
 import SwiftUI
 
+class Counter: ObservableObject{
+    
+    @Published var day = 0
+    @Published var hours = 0
+    @Published var minutes = 0
+    @Published var seconds = 0
+    
+    var selectedDate = Date()
+    
+    init() {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
+            timer in
+            
+            let calendar = Calendar.current
+            let components = calendar.dateComponents([
+                .year, .day, .month, .hour, .minute, .second], from: Date())
+            let currentDate = calendar.date(from: components)
+            print(currentDate)
+            
+            var eventDateComponents = DateComponents()
+            eventDateComponents.year = 2022
+            eventDateComponents.month = 12
+            eventDateComponents.day = 25
+            eventDateComponents.hour = 11
+            eventDateComponents.minute = 0
+            eventDateComponents.second = 0
+            
+            let eventDate = calendar.date(from: eventDateComponents)
+            print(eventDate)
+            
+            let timeLeft = calendar.dateComponents([ .day, .hour, .minute, .second], from: currentDate!, to: eventDate!)
+            
+            self.day = timeLeft.day ?? 0
+            self.hours = timeLeft.hour ?? 0
+            self.minutes = timeLeft.minute ?? 0
+            self.seconds = timeLeft.second ?? 0
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @StateObject var counter = Counter()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello iOS").padding(20)
+            HStack {
+                Text("\(counter.day) dias")
+                Text("\(counter.seconds) horas")
+                Text("\(counter.minutes) min")
+                Text("\(counter.seconds) seg")
+            }
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,10 @@
+# An iOS app sample SwiftUI
+
+* iOS
+* Swift
+* SwiftUI
+
+<img src="screen.png" width="50%" />
+
+### Reference
+* https://www.youtube.com/watch?v=t8JI3Ur51zY
